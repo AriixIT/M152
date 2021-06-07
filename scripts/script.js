7c56201b1f51f09e03e88235bfbbdb56ec3cd37c
Amin <aminhaidarprivat@gmail.com>
2021-06-07 22:10:27 +0200

    worked on project (media and animations)

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 var slideIndex = 1;
 const slides = ['resources/carousel_images/pic1.jpg', 'resources/carousel_images/pic2.jpg', 'resources/carousel_images/pic3.jpg']
 showSlides(slideIndex);
+drawSomething();
 
 function plusSlides(n) {
     slideIndex += n;
@@ -10,7 +11,12 @@ function plusSlides(n) {
 }
 
 function showSlides() {
-    console.log(document.getElementById('imageCarousel'));
     document.getElementById('imageCarousel').src = slides[slideIndex - 1];
     document.getElementById('imageCarousel').style.display = "block";
+}
+
+function drawSomething() {
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+    ctx.fillRect(10, 50, 100, 200);
 }
